eae2c5ec075c2138293824497c7fbb369bc252ef
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-09-18 11:47:30 +0530

    Modified additional logged info

--- src/hash_table.c
+++ src/hash_table.c
@@ -17,14 +17,29 @@ int insert(int value)
 
 	while (arr[index] != -1) {
 		index = (index + 1) % HASHSIZE;
-		printf("Key : %d, index: %d\n", key, index);
 		if (index == key) {
 			printf("Hash Table full!\n");
 			return 0;
 		}
 	}
-	printf("Key : %d, index: %d\n", key, index);
 	arr[index] = value;
+	return 1;
+}
+/**
+ * Function will delete the element value and set -1 at the 
+ * place and return 1 on successful operation,
+ * while return 0 while failed to find or delete the value.
+ */
+int delete (int value)
+{
+	int key = value % HASHSIZE;
+	int index = key;
+	while (arr[index] != value) {
+		index = (index + 1) % HASHSIZE;
+		if (index == key)
+			return 0;
+	}
+	arr[index] = -1;
 	return 1;
 }
 
@@ -42,5 +57,17 @@ int main()
 	if (insert(value) == 1) {
 		printf("Data %d added to Table.\n", value);
 	}
+
+	if (delete (40) == 1) {
+		printf("Data Deleted!\n");
+	} else {
+		printf("Data not found is hash_table!\n");
+	}
+	if (delete (100) == 1) {
+		printf("Data Deleted!\n");
+	} else {
+		printf("Data not found in hash_table!\n");
+	}
+
 	return EXIT_SUCCESS;
 }
