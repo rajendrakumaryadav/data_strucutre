1695dca25c7e6252289e19ed6b67affd43066943
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-10-16 08:08:09 +0530

    Added bubble_sort.c

--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -5,20 +5,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 5
 
-int main() {
-    int arr[MAX_SIZE] = {40, 20, 120, 300, 90};
+void swap(int *pInt, int *pInt1);
 
-    for (int i = 0; i < MAX_SIZE; i++) {
-        for (int j = 0; j < i; j++) {
-            arr[i] = arr[i+j];
+void bubble_sort(int arr[], int size) {
+    int i, j, flag;
+    for (int i = 0; i < size - 1; i++) {
+        flag = 0;
+        for (j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                flag = 1;
+            }
+        }
+        if (flag == 0) {
+            break;
         }
     }
+}
 
-    for (int i = 0; i < MAX_SIZE; ++i) {
-        printf("%d", arr[i]);
-    }
+void swap(int *pInt, int *pInt1) {
+    int *temp;
+    temp = pInt;
+    pInt1 = pInt;
+    pInt1 = temp;
+}
+
+int main() {
+    int arr[] = {40, 20, 120, 300, 90, 101};
+    int size = sizeof(arr) / sizeof(int);
+    bubble_sort(arr, size);
+
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 
     return EXIT_SUCCESS;
 }
